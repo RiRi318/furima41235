6f87d20ecab3f31e5c60add29d51ab228ca1f9ec
RiRi318 <r.contination@gmail.com>
2024-10-26 20:08:09 +0900

    ユーザー管理機能の実装

---
+++ app/models/user.rb
@@ -0,0 +1,23 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  VALID_FULL_WIDTH_REGEX = /\A[ぁ-んァ-ン一-龥々ー]+\z/
+  VALID_FULL_WIDTH_KATAKANA_REGEX = /\A[ァ-ンヽヾ゛゜ー]+\z/
+
+  validates :nickname, presence: true
+  validates :birthday, presence: true
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers.' }
+  validates :lastname, presence: true
+  validates :lastname, format: { with: VALID_FULL_WIDTH_REGEX, message: 'is invalid. Input full-width characters.' }
+  validates :firstname, presence: true
+  validates :firstname, format: { with: VALID_FULL_WIDTH_REGEX, message: 'is invalid. Input full-width characters.' }
+  validates :katakana_lastname, presence: true
+  validates :katakana_lastname, format: { with: VALID_FULL_WIDTH_KATAKANA_REGEX, message: 'is invalid. Input full-width katakana characters.' }
+  validates :katakana_firstname, presence: true
+  validates :katakana_firstname, format: { with: VALID_FULL_WIDTH_KATAKANA_REGEX, message: 'is invalid. Input full-width katakana characters.' }  
+
+end
