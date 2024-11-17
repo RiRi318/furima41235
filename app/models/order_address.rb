2537bb55c36cdb11720e8328c740991a1b202397
RiRi318 <r.contination@gmail.com>
2024-11-17 16:54:08 +0900

    最終課題提出の修正　１

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -8,12 +8,10 @@ class OrderAddress
   with_options presence: true do
     validates :item_id
     validates :user_id
-    validates :postal_code
     validates :postal_code, format: {with: VALID_POSTAL_CODE_REGEX, message: "is invalid. Include hyphen(-)"}
     validates :region_id, numericality: {other_than: 1, message: "can't be blank"}
     validates :region_city
     validates :street_address
-    validates :call
     validates :call, format: { with: VALID_CALL_REGEX,message: "is invalid. Please enter numbers only"}
     validates :token
   end
